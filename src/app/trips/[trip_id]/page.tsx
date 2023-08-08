afb619a1104e2fe12041a9fa7ec7041f9c645883
Gui Silva <adrianon10@gmail.com>
2023-08-08 04:06:57 -0300

    feat: add page trips /trips/:trip_id

---
+++ src/app/trips/[trip_id]/page.tsx
@@ -0,0 +1,30 @@
+import { TripDescription } from '@/components/trip-descriprion'
+import { TripHeader } from '@/components/trip-header'
+import { TripReservation } from '@/components/trip-reservation'
+import { prisma } from '@/lib/prisma'
+
+const getTripById = async (trip_id: string) => {
+  const trip = await prisma.trip.findUnique({
+    where: {
+      id: trip_id,
+    },
+  })
+
+  return trip
+}
+
+const TripDetails = async ({ params }: { params: { trip_id: string } }) => {
+  const trip = await getTripById(params.trip_id)
+
+  if (!trip) return null
+
+  return (
+    <div className="container mx-auto">
+      <TripHeader trip={trip} />
+      <TripReservation trip={trip} />
+      <TripDescription trip={trip} />
+    </div>
+  )
+}
+
+export default TripDetails
