aa1e54da38f2bff0ec4b60f360faed5382b1049e
Gui Silva <adrianon10@gmail.com>
2023-08-24 05:07:35 -0300

    feat: add trip search validation

---
+++ src/validations/trip-search-validation.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod'
+
+export const tripSearchValidation = z.object({
+  destination: z
+    .string({
+      invalid_type_error: 'Destino é obrigatório',
+      required_error: 'Destino é obrigatório',
+    })
+    .nonempty('Destino é obrigatório. Ex: Sào Paulo'),
+  start_date: z
+    .date({
+      invalid_type_error: 'Selecione data de início',
+      required_error: 'Selecione data de início',
+    })
+    .optional(),
+  budget: z.coerce
+    .number({
+      invalid_type_error: 'Orçamento é obrigatório',
+      required_error: 'Orçamento é obrigatório',
+    })
+    .optional(),
+})
+
+export type TripSearchValidationData = z.infer<typeof tripSearchValidation>
