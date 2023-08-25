68ec2f9d746ef70ee151c0734d811a98b4ac5705
Gui Silva <adrianon10@gmail.com>
2023-08-25 03:03:00 -0300

    feat: add quick search

--- src/components/quick-search.tsx
+++ src/components/quick-search.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const QuickSearch = () => {
   return (
@@ -12,7 +13,10 @@ export const QuickSearch = () => {
       </div>
 
       <div className="mt-5 flex w-full justify-between">
-        <button className="flex flex-col items-center justify-center gap-1">
+        <Link
+          href="/trips/search?destination=hotel"
+          className="flex flex-col items-center justify-center gap-1 transition-colors hover:text-primary-normal"
+        >
           <Image
             src="/hotel-icon.png"
             height={35}
@@ -20,24 +24,43 @@ export const QuickSearch = () => {
             alt="Hotel icon"
           />
           <p className="text-sm text-textColor-darker">Hotel</p>
-        </button>
-        <button className="flex flex-col items-center justify-center gap-1">
-          <Image src="/farm-icon.png" height={35} width={35} alt="Hotel icon" />
+        </Link>
+        <Link
+          href="/trips/search?destination=fazenda"
+          className="flex flex-col items-center justify-center gap-1 transition-colors hover:text-primary-normal"
+        >
+          <Image
+            src="/farm-icon.png"
+            height={35}
+            width={35}
+            alt="icone Fazenda"
+          />
           <p className="text-sm text-textColor-darker">Fazenda</p>
-        </button>
-        <button className="flex flex-col items-center justify-center gap-1">
+        </Link>
+        <Link
+          href="/trips/search?destination=chalé"
+          className="flex flex-col items-center justify-center gap-1 transition-colors hover:text-primary-normal"
+        >
           <Image
             src="/cottage-icon.png"
             height={35}
             width={35}
-            alt="Hotel icon"
+            alt="icone Chalé"
           />
           <p className="text-sm text-textColor-darker">Chalé</p>
-        </button>
-        <button className="flex flex-col items-center justify-center gap-1">
-          <Image src="/inn-icon.png" height={35} width={35} alt="Hotel icon" />
+        </Link>
+        <Link
+          href="/trips/search?destination=pousada"
+          className="flex flex-col items-center justify-center gap-1 transition-colors hover:text-primary-normal"
+        >
+          <Image
+            src="/inn-icon.png"
+            height={35}
+            width={35}
+            alt="icone Pousada"
+          />
           <p className="text-sm text-textColor-darker">Pousada</p>
-        </button>
+        </Link>
       </div>
     </div>
   )
