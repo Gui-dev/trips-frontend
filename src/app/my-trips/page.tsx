5a59c2fa184748e96abc6975b638450152e61215
Gui Silva <adrianon10@gmail.com>
2023-08-30 05:04:26 -0300

    style: add adapt my trips page to desktop

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -24,29 +24,31 @@ const MyTrips = async () => {
   })
 
   return (
-    <section className="container mx-auto p-5">
+    <section className="container mx-auto p-5 lg:max-w-[948px] lg:p-0">
       <h1 className="text-xl font-semibold text-primary-darker">
         Minhas Viagens
       </h1>
 
       {trips && trips.length < 1 && (
         <div className="flex-2 mt-11 flex flex-col items-center justify-center gap-3">
-          <p className="text-lg font-medium text-primary-darker">
+          <p className="text-lg font-medium text-primary-darker lg:text-xl">
             Você ainda não tem nenhuma reserva!
           </p>
           <Link
             href="/"
-            className="text-sm font-bold text-primary-normal hover:text-primary-darker"
+            className="text-sm font-bold text-primary-normal hover:text-primary-darker lg:text-base"
           >
             Reserve uma agora
           </Link>
         </div>
       )}
 
-      {trips &&
-        trips.map((trip) => {
-          return <UserReservationItem key={trip.id} reservation={trip} />
-        })}
+      <div className="flex flex-col pb-5 lg:grid lg:grid-cols-3 lg:gap-5 lg:pb-10">
+        {trips &&
+          trips.map((trip) => {
+            return <UserReservationItem key={trip.id} reservation={trip} />
+          })}
+      </div>
     </section>
   )
 }
